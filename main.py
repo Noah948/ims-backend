0b8052604ddf3e69fa37f43aba8a6de3e6ca72a6
Abdullah <as794614@gmail.com>
2026-01-10 15:34:29 +0530

    cors added

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 # main.py
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from core.database import Base, engine
 from core.config import settings
 
 from routes.user_routes import router as user_router
 from routes.auth_routes import router as auth_router
+
 # Create database tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.PROJECT_NAME)
+
+# ✅ CORS CONFIGURATION
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router)
 app.include_router(auth_router)
 
